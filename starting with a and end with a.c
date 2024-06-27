349496fd6221af0134ec5a83d764f243772d17a2
mahi5062 <124763253+mahi5062@users.noreply.github.com>
2024-06-27 08:53:42 +0530

    Add files via upload

---
+++ starting with a and end with a.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_LEN 100
+
+int main()
+{
+    char input_string[MAX_LEN];
+    int state = 0;
+    int i;
+
+    printf("Enter a string: ");
+    scanf("%s", input_string);
+
+    int len = strlen(input_string);
+    for ( i = 0; i < len; i++) {
+        if (state == 0 && input_string[i] == 'a')
+            state = 1;
+        else if (state == 0 && input_string[i] != 'a')
+            state = 2;
+    }
+
+    if (state == 1 && input_string[len - 1] == 'a')
+        printf("Accepted\n");
+    else
+        printf("Rejected\n");
+
+    return 0;
+}
